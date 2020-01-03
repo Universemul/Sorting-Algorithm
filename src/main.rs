d2f2624abcd2deeaaa3d080c0df6a59d3fe56942
Universemul <davidbarthelemy28@gmail.com>
2020-01-04 00:12:54 +0100

    add insertion sort

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ pub mod algorithms;
 fn main() {
     let data: Vec<i32> = vec![4, 2, 3, 1, 5];
     algorithms::bubble::sort(&mut data.clone());
-    algorithms::selection::sort(&mut data.clone())
+    algorithms::selection::sort(&mut data.clone());
+    algorithms::insertion::sort(&mut data.clone())
 }
