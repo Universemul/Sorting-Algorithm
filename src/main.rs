a76a2b42291430d784173ab4617f6d17cfa54c3b
Universemul <davidbarthelemy28@gmail.com>
2020-01-03 23:49:01 +0100

    add bubble and selection sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+pub mod algorithms;
+
 fn main() {
-    println!("Hello, world!");
+    let data: Vec<i32> = vec![4, 2, 3, 1, 5];
+    algorithms::bubble::sort(&mut data.clone());
+    algorithms::selection::sort(&mut data.clone())
 }
