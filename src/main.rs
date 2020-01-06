dc9a843bf5e47890a9ef0f793e746d21789b8044
Universemul <davidbarthelemy28@gmail.com>
2020-01-07 00:08:41 +0100

    add merge sort

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 pub mod algorithms;
 
 fn main() {
-    let mut data: Vec<i32> = vec![4, 2, 3, 1, 5];
+    let mut data: Vec<i32> = vec![4, 2, 3, 1, 5, 9, -1];
     //algorithms::bubble::sort(&mut data);
     //algorithms::selection::sort(&mut data);
     //algorithms::insertion::sort(&mut data);
     println!("{:?}", data);
-    algorithms::quicksort::sort(&mut data);
+    algorithms::mergesort::sort(&mut data);
     println!("{:?}", data);
 }
