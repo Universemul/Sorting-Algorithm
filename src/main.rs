0a48bed2b49b45558b747a02ca137a5bb3d29896
Universemul <davidbarthelemy28@gmail.com>
2020-01-09 23:04:47 +0100

    remove main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
 pub mod algorithms;
 
 fn main() {
-    let mut data: Vec<i32> = vec![4, 2, 3, 1, 5, 9, -1];
-    //algorithms::bubble::sort(&mut data);
-    //algorithms::selection::sort(&mut data);
-    //algorithms::insertion::sort(&mut data);
-    println!("{:?}", data);
-    algorithms::heapsort::sort(&mut data);
-    println!("{:?}", data);
 }
